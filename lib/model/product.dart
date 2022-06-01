99b400791600f7f7fc4be578edaacd5edf0dc604
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-01 23:26:18 +0530

    feat: model of the product

---
+++ lib/model/product.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+List<Product> productFromJson(String str) =>
+    List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+
+String productToJson(List<Product> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Product with ChangeNotifier {
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  int id;
+  String title;
+  double price;
+  String description;
+  String category;
+  String image;
+  Rating rating;
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"].toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+        rating: Rating.fromJson(json["rating"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+        "rating": rating.toJson(),
+      };
+}
+
+class Rating {
+  Rating({
+    required this.rate,
+    required this.count,
+  });
+
+  double rate;
+  int count;
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: json["rate"].toDouble(),
+        count: json["count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "rate": rate,
+        "count": count,
+      };
+}
