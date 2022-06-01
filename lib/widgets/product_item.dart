15ca18d943fc3787ede8b639da8051d067f4a02b
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-02 03:56:06 +0530

    refactor: path name

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,4 @@
-import 'package:fashion_store/model/Product.dart';
+import 'package:fashion_store/model/product.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
