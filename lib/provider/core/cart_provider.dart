a68e53474e69aa3abac341b532ce51977042d152
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-05 02:56:27 +0530

    feat: add increment/decrement functions

--- lib/provider/core/cart_provider.dart
+++ lib/provider/core/cart_provider.dart
@@ -2,25 +2,29 @@ import 'package:fashion_store/model/product.dart';
 import 'package:flutter/material.dart';
 
 class CartProvider with ChangeNotifier {
+  // counter for cart items
   int _counter = 0;
   int get counter => _counter;
 
+  // grand total price of the cart items
   double _totalPrice = 0.0;
   double get totalPrice => _totalPrice;
 
   List<Product> list = <Product>[];
 
-  addProduct(
-      int id, String title, double price, String description, String image) {
+  addProduct(int id, int quantity, String title, double price,
+      String description, String image) {
     list.add(
       Product(
         id: id,
+        quantity: quantity,
         title: title,
         price: price,
         description: description,
         image: image,
       ),
     );
+    addCounter();
     notifyListeners();
   }
 
@@ -43,6 +47,20 @@ class CartProvider with ChangeNotifier {
     return _totalPrice;
   }
 
+  void addQuantity(int index) {
+    list[index].quantity++;
+    notifyListeners();
+  }
+
+  void subtractQuantity(int index) {
+    list[index].quantity--;
+    notifyListeners();
+  }
+
+  int getCounter() {
+    return _counter;
+  }
+
   void addCounter() {
     _counter++;
     notifyListeners();
@@ -51,9 +69,5 @@ class CartProvider with ChangeNotifier {
   void subtractCounter() {
     _counter--;
     notifyListeners();
-  }
-
-  int getCounter() {
-    return _counter;
   }
 }
