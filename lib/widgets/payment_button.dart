b53b72dbdee7dd9d0c0507bbb3dedeb4e17f1ebd
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-13 22:13:28 +0530

    feat: add toast message on button click

--- lib/widgets/payment_button.dart
+++ lib/widgets/payment_button.dart
@@ -9,22 +9,42 @@ class PaymentButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        width: double.infinity,
-        height: 65,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(7),
+    return ElevatedButton(
+      onPressed: () {
+        var snackBar = SnackBar(
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: Colors.green,
+            content: Row(
+              children: [
+                const Icon(
+                  Icons.check_circle,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: SizeConfig.safeHorizontal! * .02,
+                ),
+                const Text('Payment successful!'),
+              ],
+            ),
+            duration: Duration(seconds: 2));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        elevation: 0,
+        fixedSize: Size(
+            SizeConfig.safeHorizontal! * .85, SizeConfig.safeVertical! * .07),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(7.0),
         ),
-        child: const Center(
-          child: Text(
-            "Payment",
-            style: TextStyle(
-                fontSize: 17, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-        ),
+      ),
+      child: Text(
+        "Payment",
+        textAlign: TextAlign.center,
+        style: Theme.of(context)
+            .textTheme
+            .headline6!
+            .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
       ),
     );
   }
